3d714741d7a2154e3e0dc34451a39afac30d480b
kumkum <kumkumks671@gmail.com>
2024-12-09 10:30:20 +0530

    created three java file

--- java/stringgs.java
+++ java/stringgs.java
@@ -1,7 +1,9 @@
 import java.util.*;
 public class stringgs{
 public static void main(String[]args){
-    String name="KumKum";
+    System.out.println("Enter your name: ");
+    Scanner sc=new Scanner(System.in);
+    String name=sc.nextLine();
     System.out.println(name);
 }
 }
